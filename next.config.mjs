3c83c519aea23be7c38ac2c1329dad06dfbc7671
Aroooba <arooba.shahoor@gmail.com>
2024-05-31 14:37:49 +0900

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,26 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     typescript: {
-//         // Ignore build errors
-//         ignoreBuildErrors: true,
-//     },
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: '0.0.0.0',
-//                 port: process.env.PORT || '8000',
-//                 pathname: '/**'
-//             }
-//         ]
-//     }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // typescript: {
+    //     // Ignore build errors
+    //     ignoreBuildErrors: true,
+    // },
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: '0.0.0.0',
+    //             port: process.env.PORT || '8000',
+    //             pathname: '/**'
+    //         }
+    //     ]
+    // }
 
-//     // next.config.js
-// }
-
-// export default nextConfig;
-
-module.exports = {
     server: {
         host: '0.0.0.0',
         port: process.env.PORT || 3000,
     },
-};
+
+    // next.config.js
+}
+
+export default nextConfig;
